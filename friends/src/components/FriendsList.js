cacfea3f12ea1dd53e02879e6b6d36a517e77769
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-12-17 14:58:27 -0500

    friendslist file done

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+
+const FriendsList = () => {
+  
+const [friend,setFriend] = useState([]);
+    
+axiosWithAuth()
+.get('/friends')
+.then(res => {
+    setFriend(res.data)
+    console.log(res.data, 'friendslist')
+})
+.catch(err => {
+    console.log(err)
+})
+
+if(friend === []) {
+    return(
+        <p>Loading friends...</p>
+    )
+}
+
+return (
+    <div>
+        <h1>Friends List</h1>
+
+        {friend.map(f => {
+            return(
+                <div>
+                    <h3>{f.name}</h3>
+                    <h3>{f.age}</h3>
+                    <h3>{f.email}</h3>
+                </div>
+            )
+        })}
+    </div>
+  )
+}
+
+export default FriendsList;
