8a7978682f9441944de7bba4368eb2227688b2e7
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-12-17 14:44:18 -0500

    filling out app file with private route

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,25 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Link to='/login'>Login</Link>
+      <Link to='/protected'>Protected Page</Link>
+      <Switch>
+        <PrivateRoute exact path='/protected' component={}/>
+        <Route path='/login' component={Login}/>
+      </Switch>
     </div>
+    </Router>
   );
 }
+
+{/* <header className="App-header">
+<img src={logo} className="App-logo" alt="logo" />
+<p>
+  Edit <code>src/App.js</code> and save to reload.
+</p>
+<a
+  className="App-link"
+  href="https://reactjs.org"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  Learn React
+</a>
+</header> */}
 
 export default App;
